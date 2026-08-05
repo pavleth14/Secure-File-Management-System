5e2a2a7ea61e818a9f56e92d498b9173fee02d65
magda-tbf <magdatwobrothersfreight@gmail.com>
2026-08-05 23:00:26 +0200

    removed debugging mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { UploadProvider } from './context/UploadContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
         <AuthProvider>
